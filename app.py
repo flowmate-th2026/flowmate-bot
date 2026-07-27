7866c3ae78613f9431744d8da95885ae6b77fd5f
flowmate-th2026 <flowmate.th@gmail.com>
2026-07-27 07:50:14 +0700

    Fix indentation for handle_message function

--- app.py
+++ app.py
@@ -25,16 +25,15 @@ def home():
 @app.route("/callback", methods=["POST"])
 def callback():
     signature = request.headers["X-Line-Signature"]
-
     body = request.get_data(as_text=True)
 
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         return "Invalid signature", 400
-
     return "OK"
-    @handler.add(MessageEvent, message=TextMessage)
+    
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_message = event.message.text
 
