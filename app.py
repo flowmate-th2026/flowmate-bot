684eba73c6be1f0390b6db7bd8bba10bb0c22192
flowmate-th2026 <flowmate.th@gmail.com>
2026-07-27 07:28:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,52 @@
 import os
+
 from flask import Flask, request
+from dotenv import load_dotenv
+
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent,
+    TextMessage,
+    TextSendMessage,
+)
 app = Flask(__name__)
+load_dotenv()
+
+line_bot_api = LineBotApi(
+    os.getenv("CHANNEL_ACCESS_TOKEN")
+)
+handler = WebhookHandler(
+    os.getenv("CHANNEL_SECRET")
+)
 @app.route("/")
 def home():
     return "Flowmate Bot is running!"
 @app.route("/callback", methods=["POST"])
 def callback():
+    signature = request.headers["X-Line-Signature"]
+
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        return "Invalid signature", 400
+
     return "OK"
+    @handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_message = event.message.text
+
+    if user_message == "สวัสดี":
+        reply = "👋 สวัสดีค่ะ ยินดีต้อนรับสู่ FlowMate"
+    else:
+        reply = f"คุณพิมพ์ว่า: {user_message}"
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply)
+    )
 if __name__== "__main__":
     app.run(
         host="0.0.0.0",
