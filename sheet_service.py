d231e073ccbe7de229fc22ab5855d9145ffaef80
flowmate-th2026 <flowmate.th@gmail.com>
2026-07-28 11:22:14 +0700

    day 7 add google sheets connection
    Added functions to interact with Google Sheets for sales data.

--- sheet_service.py
+++ sheet_service.py
@@ -1,1 +1,28 @@
+import gspread
 
+from config import GOOGLE_CREDENTIALS_FILE, GOOGLE_SHEET_ID
+
+
+def get_sales_worksheet():
+    google_client = gspread.service_account(
+        filename=GOOGLE_CREDENTIALS_FILE
+    )
+
+    spreadsheet = google_client.open_by_key(GOOGLE_SHEET_ID)
+    worksheet = spreadsheet.get_worksheet(0)
+
+    return worksheet
+
+
+def append_sale_row(date_text, time_text, amount):
+    worksheet = get_sales_worksheet()
+
+    worksheet.append_row(
+        [
+            date_text,
+            time_text,
+            "ยอดขาย",
+            amount,
+        ],
+        value_input_option="USER_ENTERED",
+    )
