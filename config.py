c827cb3b3ae691ff854f2e70f188a62c54f9597e
flowmate-th2026 <flowmate.th@gmail.com>
2026-07-28 11:20:19 +0700

    day 7 add google sheets config

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ load_dotenv()
 
 CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
+
+GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
+GOOGLE_CREDENTIALS_FILE = "/etc/secrets/google-credentials.json"
