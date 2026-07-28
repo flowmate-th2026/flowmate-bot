1253d0cac7c64b773f341f79872aa1f94a3c2236
flowmate-th2026 <flowmate.th@gmail.com>
2026-07-28 11:23:55 +0700

    day 7 add sales recording service

--- services.py
+++ services.py
@@ -1,1 +1,19 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
+from sheet_service import append_sale_row
+
+
+def record_sale(amount):
+    thailand_time = datetime.now(ZoneInfo("Asia/Bangkok"))
+
+    date_text = thailand_time.strftime("%d/%m/%Y")
+    time_text = thailand_time.strftime("%H:%M")
+
+    append_sale_row(
+        date_text=date_text,
+        time_text=time_text,
+        amount=amount,
+    )
+
+    return date_text, time_text
