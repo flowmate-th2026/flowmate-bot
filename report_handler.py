a76b922db9fc2fec60aee19161ea45a965025226
flowmate-th2026 <flowmate.th@gmail.com>
2026-07-30 09:42:54 +0700

    Update report_handler.py

--- report_handler.py
+++ report_handler.py
@@ -2,8 +2,10 @@ from services import (
     get_daily_sales,
     get_daily_expense,
     get_daily_customer,
+    get_thailand_time,
 )
 
+from product_handler import get_top_products_data
 from report_flex import create_report_flex
 
 
@@ -16,11 +18,16 @@ def handle_report_message():
     expense = get_daily_expense()
     customer = get_daily_customer()
 
+    now = get_thailand_time()
+    date_text = now.strftime("%d/%m/%Y")
+    top_products = get_top_products_data(date_text, limit=3)
+
     profit = sales - expense
 
     return create_report_flex(
-        sales=sales,
-        expense=expense,
-        customer=customer,
-        profit=profit,
-    )
+    sales=sales,
+    expense=expense,
+    customer=customer,
+    profit=profit,
+    top_products=top_products,
+)
